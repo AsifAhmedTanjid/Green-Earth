467bdd7fc22ad3f24cf2d545b60155ccb12dfb1b
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-09-07 20:54:27 +0600

    category list fetched and displayed in the category section

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,22 @@
 console.log('connected');
+
+
+//load category
+
+loadCategory =()=>{
+    fetch("https://openapi.programming-hero.com/api/categories")
+    .then(res=> res.json())
+    .then(data=>displayCategories(data.categories))
+}
+displayCategories=(categories)=>{
+    const categoryContainer=document.getElementById("catergory-container");
+    console.log(categoryContainer);
+    for(let category of categories){
+        // console.log(category);
+        const btnDiv = document.createElement('div');
+        btnDiv.innerHTML=`<button class="w-full text-left py-1">${category.category_name}</button>`;
+        categoryContainer.appendChild(btnDiv);
+        
+    }
+}
+loadCategory();
