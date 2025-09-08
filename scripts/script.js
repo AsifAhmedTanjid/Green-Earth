d4f672f043e28beb97595dc64b9bbf07f34cd7e2
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-09-08 13:27:48 +0600

    All tree data fetched and displayed as cards in card section

--- scripts/script.js
+++ scripts/script.js
@@ -10,7 +10,7 @@ loadCategory =()=>{
 }
 displayCategories=(categories)=>{
     const categoryContainer=document.getElementById("catergory-container");
-    console.log(categoryContainer);
+    // console.log(categoryContainer);
     for(let category of categories){
         // console.log(category);
         const btnDiv = document.createElement('div');
@@ -20,3 +20,60 @@ displayCategories=(categories)=>{
     }
 }
 loadCategory();
+
+
+
+// load cards 
+
+const loadCard = async()=>{
+    const url="https://openapi.programming-hero.com/api/plants";
+    const res =await fetch(url);
+    const data = await res.json();
+    displayCard(data.plants);
+    
+}
+
+displayCard = (cards) =>{
+    const cardContainer =document.getElementById('card-container');
+    for(let card of cards){
+        console.log(card);
+        
+        const cardDiv =document.createElement("div");
+        cardDiv.innerHTML=`            <!-- card-1  -->
+            <div class="card bg-base-100  shadow-sm">
+              <figure class="p-4 pb-0 -mb-2">
+                <img
+                  src="${card.image}"
+                  alt="Product"
+                  class="rounded-xl h-[250px] w-[300px]"
+                />
+              </figure>
+              <div class="card-body">
+                <h2 class="card-title font-semibold text-sm mb-1">
+                  ${card.name}
+                </h2>
+                <p class="text-xs text-[#71717A]/80 text-justify mb-1">
+                 ${card.description}
+                </p>
+                <div class="flex justify-between font-semibold text-sm">
+                  <div
+                    class="badge text-[#15803D] text-sm font-medium rounded-full bg-[#DCFCE7] py-1 px-3 mb-2"
+                  >
+                    ${card.category}
+                  </div>
+                  <div>৳ <span>${card.price}</span></div>
+                </div>
+                <div class="card-actions justify-center">
+                  <button
+                    class="text-white bg-[#15803D] p-2 px-3 lg:px-5 hover:cursor-pointer rounded-full text-base font-medium hover:bg-green-900 w-full"
+                  >
+                    Add to Cart
+                  </button>
+                </div>
+              </div>
+            </div>`;
+            cardContainer.appendChild(cardDiv);
+    }
+
+}
+loadCard();
