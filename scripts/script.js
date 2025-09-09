908fabc8b6e664132e5597633bbbfc84f22e4e03
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-09-09 14:49:58 +0600

    add to cart implemented with quantity

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,11 @@
 console.log("connected");
 
+let cart = [];
+let allPlants = [];
+
 //load category
 
-const loadCategory = async() => {
+const loadCategory = async () => {
   // fetch("https://openapi.programming-hero.com/api/categories")
   //   .then((res) => res.json())
   //   .then((data) => displayCategories(data.categories));
@@ -13,7 +16,7 @@ const loadCategory = async() => {
   removeActiveCategory();
   const clickedBtn = document.getElementById("allTreeBtn");
   // console.log(clickedBtn);
-  
+
   clickedBtn.classList.add("activeCategory");
 };
 const displayCategories = (categories) => {
@@ -54,6 +57,8 @@ const loadCard = async () => {
   const url = "https://openapi.programming-hero.com/api/plants";
   const res = await fetch(url);
   const data = await res.json();
+  allPlants = data.plants;
+  // console.log(allPlants);
   displayCard(data.plants);
 };
 
@@ -89,8 +94,8 @@ const displayCard = (cards) => {
                   <div>৳ <span>${card.price}</span></div>
                 </div>
                 <div class="card-actions justify-center">
-                  <button
-                    class="text-white bg-[#15803D] p-2 px-3 lg:px-5 hover:cursor-pointer rounded-full text-base font-medium hover:bg-green-900 w-full"
+                  <button onclick="addToCart(${card.id})"
+                    class="add-to-cart-btn text-white bg-[#15803D] p-2 px-3 lg:px-5 hover:cursor-pointer rounded-full text-base font-medium hover:bg-green-900 w-full"
                   >
                     Add to Cart
                   </button>
@@ -99,6 +104,62 @@ const displayCard = (cards) => {
             </div>`;
     cardContainer.appendChild(cardDiv);
   }
+};
+//cart
+
+const addToCart = (id) => {
+  const plant = allPlants.find((p) => p.id === id);
+  const existing = cart.find((item) => item.id === id);
+
+  let totalPrice = 0;
+
+  if (existing) {
+    existing.quantity += 1;
+  } else {
+    cart.push({ ...plant, quantity: 1 });
+  }
+  // console.log(plant);
+
+  const cartContainer = document.getElementById("cart-container");
+  cartContainer.innerHTML = "";
+  const totalPriceDiv = document.createElement("div");
+  cart.forEach((item) => {
+    console.log(item);
+    const cartItem = document.createElement("div");
+
+    cartItem.innerHTML = `
+    
+    
+              <div class="bg-[#F0FDF4] flex justify-between items-center px-3 py-2 mb-2">
+         
+              <div>      
+                <h3 class="font-semibold text-sm">${item.name}</h1>
+                <p class="text-[#1F2937]/50">৳ <span class="tree-price">${item.price}</span> x <span class="tree-quantity">${item.quantity}</span></p>
+              </div>
+              <div>
+                <p class="delete-from-cart text-[#8C8C8C]">X</p>
+              </div>
+  
+            </div>
+    
+    
+    
+    `;
+    cartContainer.appendChild(cartItem);
+    totalPrice = totalPrice + item.price * item.quantity;
+  });
+
+  totalPriceDiv.innerHTML = `
+          <!-- <div> -->
+            <hr class="opacity-10">
+          <!-- </div> -->
+
+          <div class="flex justify-between mt-2 text-[#1F2937] font-medium">
+            <h3>Total:</h3>
+            <p>৳ <span class="total-price">${totalPrice}</span></p>
+          </div>`;
+
+  cartContainer.appendChild(totalPriceDiv);
 };
 
 //modal
@@ -126,13 +187,13 @@ const displayTreeModal = (plant) => {
                  <b>Name:</b> ${plant.name}
                 </h2>
                 <p class="text-base text-Black text-justify mb-1">
-                 <b>Description</b> : ${plant.description}
+                 <b>Description : </b>${plant.description}
                 </p>
                 
                   <div
                     class="py-1 mb-2 text-base text-Black"
                   >
-                    <b>Category :</b> ${plant.category}
+                    <b>Category : </b>${plant.category}
                   </div>
                   <div class="py-1 text-base text-Black"><b>Price :</b> ৳ <span>${plant.price}</span></div>
                 
